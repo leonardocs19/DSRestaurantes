41a84f24608112c1b955a57c54341b9a398c7034
Leonardo Carvalho <ra93852@uem.br>
2017-11-17 21:57:05 -0200

    Integração Ajuda Online Estoque

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -8,6 +8,7 @@ import CrudFuncionario from '../../ui/CrudFuncionario.jsx';
 import CrudItem from '../../ui/CrudItem.jsx';
 import CrudFinanca from '../../ui/CrudFinanca.jsx';
 import AjudaFuncionario from '../../ui/AjudaFuncionario.jsx';
+import AjudaEstoque from '../../ui/AjudaEstoque.jsx';
 
 
 const browserHistory = createBrowserHistory();
@@ -20,6 +21,7 @@ export const renderRoutes = () => (
       <Route exact path="/item" component={CrudItem}/>
       <Route exact path="/financa" component={CrudFinanca}/>
       <Route exact path="/funcionario/ajuda" component={AjudaFuncionario}/>
+      <Route exact path="/item/ajuda" component={AjudaEstoque}/>
     </div>
   </Router>
 );
--- imports/ui/AjudaEstoque.jsx
+++ imports/ui/AjudaEstoque.jsx
@@ -10,26 +10,26 @@ export default class App extends Component {
 
         <div class="panel panel-info">
           <div class="panel-heading">Para cadastro de estoque:</div>
-          <div class="panel-body"><p>Insira os dados correspondentes no campos especÌficos, para nome, quantidade, data da compra, data de vencimento e o estoque mÌnimo  </p>
-          <p>Lembrado que nome, quantidade, data da compra, data de vencimento e o estoque mÌnimo s„o obrigatÛrios</p>
-          <p>quantidade e estoque mÌnimo devem ser valores n„o negativos</p>
-          <p>Nome deve conter pelo menos uma letra, a datas devem ser v·lidas </p></div>
+          <div class="panel-body"><p>Insira os dados correspondentes no campos espec√≠ficos, para nome, quantidade, data da compra, data de vencimento e o estoque m√≠nimo  </p>
+          <p>Lembrado que nome, quantidade, data da compra, data de vencimento e o estoque m√≠nimo s√£o obrigat√≥rios</p>
+          <p>quantidade e estoque m√≠nimo devem ser valores n√£o negativos</p>
+          <p>Nome deve conter pelo menos uma letra, a datas devem ser v√°lidas </p></div>
         </div>
 
         <div class="panel panel-info">
-          <div class="panel-heading">Para adiÁ„o de um item no estoque:</div>
-          <div class="panel-body"><p>Coloque valores v·lidos para cada item separadamente e depois clique em adicionar</p>
-          <p>Os itens v„o para um "carrinho de compras", onde no poder„o ainda ser editados</p>
-          <p>Depois de verificar que tudo est· correto, clique em confirmar para adicionar todas as mudanÁas</p>
-          <p>ApÛs a confirmaÁ„o final, n„o mais ser· permitida a ediÁ„o dos itens</p></div>
+          <div class="panel-heading">Para adi√ß√£o de um item no estoque:</div>
+          <div class="panel-body"><p>Coloque valores v√°lidos para cada item separadamente e depois clique em adicionar</p>
+          <p>Os itens v√£o para um "carrinho de compras", onde no poder√£o ainda ser editados</p>
+          <p>Depois de verificar que tudo est√° correto, clique em confirmar para adicionar todas as mudan√ßas</p>
+          <p>Ap√≥s a confirma√ß√£o final, n√£o mais ser√° permitida a edi√ß√£o dos itens</p></div>
         </div>
 
         <div class="panel panel-info">
-          <div class="panel-heading">Para visualizaÁ„o de estoque:</div>
-          <div class="panel-body"><p>Os estoques cadastrados s„o visualizados logo abaixo da tela de cadastro</p></div>
+          <div class="panel-heading">Para visualiza√ß√£o de estoque:</div>
+          <div class="panel-body"><p>Os estoques cadastrados s√£o visualizados logo abaixo da tela de cadastro</p></div>
         </div>
 
-        <a className="btn btn-primary btn-md" href="http://localhost:3000/estoque" role="button">Voltar</a>
+        <a className="btn btn-primary btn-md" href="http://localhost:3000/item" role="button">Voltar</a>
       </div>
     )
   }
