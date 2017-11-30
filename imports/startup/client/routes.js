2e355871f6870e5510a68770571c10bf57209756
Educaruzzo <30328949+Educaruzzo@users.noreply.github.com>
2017-11-30 21:36:30 -0200

    Atualização do arquivo de caminhos

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -8,8 +8,11 @@ import CrudFuncionario from '../../ui/CrudFuncionario.jsx';
 import CrudItem from '../../ui/CrudItem.jsx';
 import CrudFinanca from '../../ui/CrudFinanca.jsx';
 import AjudaFuncionario from '../../ui/AjudaFuncionario.jsx';
+import AjudaDespesas from '../../ui/AjudaDespesas.jsx';
 import AjudaEstoque from '../../ui/AjudaEstoque.jsx';
 import RelatorioFuncionario from '../../ui/RelatorioFuncionario.jsx';
+import RelatorioEstoque from '../../ui/RelatorioEstoque.jsx';
+import RelatorioDespesa from '../../ui/RelatorioDespesa.jsx';
 
 
 const browserHistory = createBrowserHistory();
@@ -23,7 +26,10 @@ export const renderRoutes = () => (
       <Route exact path="/financa" component={CrudFinanca}/>
       <Route exact path="/funcionario/ajuda" component={AjudaFuncionario}/>
       <Route exact path="/item/ajuda" component={AjudaEstoque}/>
+      <Route exact path="/financa/ajuda" component={AjudaDespesas}/>
       <Route exact path="/funcionario/relatorio" component={RelatorioFuncionario}/>
+      <Route exact path="/financa/relatorio" component={RelatorioDespesa}/>
+      <Route exact path="/item/relatorio" component={RelatorioEstoque}/>
     </div>
   </Router>
 );
